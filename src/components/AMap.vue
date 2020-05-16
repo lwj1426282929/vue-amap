0648d070ebc5112a3c3b30f5ac7d2bb42ca77578
lwj1426282929 <1426282929@qq.com>
2020-05-16 23:02:49 +0800

    添加点击移除marker

--- src/components/AMap.vue
+++ src/components/AMap.vue
@@ -1,95 +1,164 @@
 <template>
-  <div id="container"></div>
+    <div id="container"
+         @click="handleClick"></div>
 </template>
 
 <script>
 import AMap from 'AMap'
-// import Loca from 'Loca'
 
 export default {
-  name: 'AMap',
+    name: 'AMap',
 
-  data () {
-    return {
-      map: '',
-      markers: [
-        {
-          lng: 105.005,
-          lat: 32.349,
-          name: '1'
+    data () {
+        return {
+            map: '',
+            // 标记点
+            markers: [],
+            // 信息窗体
+            infoWindows: [],
+            // 连接线
+            polylines: []
+        }
+    },
+
+    mounted () {
+        this.map = new AMap.Map('container', {
+            features: ['bg', 'point'],
+            center: [105.005, 32.349,],
+            zoomEnable: false
+        })
+
+        this.renderMarkers()
+    },
+
+    methods: {
+        // 渲染点
+        renderMarkers () {
+            let res = [
+                { lng: 105.005, lat: 32.349, name: '1' },
+                { lng: 104.602, lat: 32.067, name: '2' },
+                { lng: 105.272, lat: 32.451, name: '3' },
+                { lng: 105.042, lat: 32.402, name: '4' },
+                { lng: 104.982, lat: 32.24, name: '5' },
+            ]
+
+            res.forEach((item, index) => {
+                let circleMarker = new AMap.CircleMarker({
+                    center: [item.lng, item.lat],
+                    radius: 10,
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 1,
+                    fillColor: '#FF0000',
+                    fillOpacity: 1,
+                    cursor: 'pointer',
+                    clickable: true
+                })
+
+                let infoWindow = new AMap.Marker({
+                    position: new AMap.LngLat(item.lng, item.lat),
+                    draggable: true,
+                    offset: new AMap.Pixel(100, -152),
+                    visible: false,
+                    topWhenClick: true,
+                    content: `<div class="info-window">
+                                  <div class="info-window-header">
+                                    <span>${item.name}</span>  
+                                    <span class="close" data-index="${index}">+</span>
+                                  </div>
+                                  <div class="info-window-body"></div>
+                              </div>`
+                })
+
+                let position = this.map.lngLatToContainer(new AMap.LngLat(item.lng, item.lat))
+                let endPoint = this.map.containerToLngLat(new AMap.Pixel(position.x + 100, position.y - 76))
+
+                let polyline = new AMap.Polyline({
+                    path: [[item.lng, item.lat], [item.lng, endPoint.lat], [endPoint.lng, endPoint.lat]],
+                    strokeColor: '#FF0000',
+                    strokeStyle: 'dashed',
+                    strokeWeight: 1,
+                    draggable: true
+                })
+                polyline.hide()
+
+                // 显示信息弹窗与连接线
+                circleMarker.on('click', () => {
+                    infoWindow.show()
+                    polyline.show()
+                })
+
+                // 信息弹窗拖拽
+                infoWindow.on('dragging', () => {
+                    // 转换成像素
+                    let endPixel = this.map.lngLatToContainer(new AMap.LngLat(infoWindow.getPosition().lng, infoWindow.getPosition().lat))
+                    // 处理像素差后在转换回坐标
+                    let endPoint = this.map.containerToLngLat(new AMap.Pixel(endPixel.x + 100, endPixel.y - 76))
+                    polyline.setPath([[item.lng, item.lat], [item.lng, endPoint.lat], [endPoint.lng, endPoint.lat]])
+                })
+
+                this.markers.push(circleMarker)
+                this.infoWindows.push(infoWindow)
+                this.polylines.push(polyline)
+            })
+
+            // 将点、窗体信息、连接线添加到图层
+            this.map.add(this.markers)
+            this.map.add(this.infoWindows)
+            this.map.add(this.polylines)
         },
-        {
-          lng: 104.602,
-          lat: 32.067,
-          name: '2'
-        },
-        {
-          lng: 105.272,
-          lat: 32.451,
-          name: '3'
-        },
-        {
-          lng: 105.042,
-          lat: 32.402,
-          name: '4'
-        },
-        {
-          lng: 104.982,
-          lat: 32.24,
-          name: '5'
-        },
-      ]
+
+        // 处理点击事件
+        handleClick (e) {
+            if (e.target.className === 'close') {
+                let index = e.target.getAttribute('data-index')
+                this.infoWindows[index].hide()
+                this.polylines[index].hide()
+            }
+        }
     }
-  },
-
-  mounted () {
-    this.map = new AMap.Map( 'container', {
-      mapStyle: 'amap://styles/dark',
-      features: ['bg', 'point'],
-      center: [105.005, 32.349,]
-    } )
-
-    this.renderMarkers()
-  },
-
-  methods: {
-    // 渲染点
-    renderMarkers () {
-      this.markers.forEach( item => {
-        let circleMarker = new AMap.CircleMarker( {
-          center: [item.lng, item.lat],
-          radius: 10,
-          strokeColor: 'white',
-          strokeWeight: 2,
-          strokeOpacity: 0.5,
-          fillColor: 'red',
-          fillOpacity: 0.5,
-          zIndex: 10,
-          bubble: true,
-          cursor: 'pointer',
-          clickable: true,
-          extData: { ...item }
-        } )
-        circleMarker.on( 'click', ( e ) => {
-          // console.log( e.target.getExtData() )
-          let target = e.target.getExtData()
-          let rectangle = new AMap.Rectangle({
-            map: this.map,
-            bounds: new AMap.Bounds(new AMap.LngLat(target.lng - 10, target.lat - 10), new AMap.LngLat(target.lng + 10, target.lat + 10)),
-            extData: target
-          })
-          rectangle.setMap(this.map)
-        } )
-        circleMarker.setMap( this.map )
-      } )
-    }
-  }
 }
 </script>
 
 <style lang="less" scoped>
 #container {
-  width: 100%;
-  height: 600px;
+    width: 100%;
+    height: 800px;
+    position: relative;
+
+    /deep/.info-window {
+        width: 300px;
+        min-height: 150px;
+        background: #fff;
+        border: 1px solid #000;
+        border-radius: 8px;
+        z-index: 999;
+        color: red;
+        cursor: move;
+        overflow: hidden;
+        box-sizing: border-box;
+
+        .info-window-header {
+            height: 30px;
+            line-height: 30px;
+            color: #fff;
+            background: #333;
+            padding: 0 5px 0 10px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .close {
+                display: block;
+                font-size: 30px;
+                cursor: pointer;
+                text-align: center;
+                transform: rotate(45deg);
+
+                &:hover {
+                    color: #409eff;
+                }
+            }
+        }
+    }
 }
 </style>
